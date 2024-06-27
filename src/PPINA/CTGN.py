de3d44aa102c239bd3f2d22c9a682f56216dfc00
mairoze <maiteleb830@gmail.com>
2024-06-27 19:49:45 +0300

    defining function which named getting_gene_name and takes argument(uniprot id) which Ids protein

--- src/PPINA/CTGN.py
+++ src/PPINA/CTGN.py
@@ -1,12 +1,7 @@
 import networkx as nx
 import numpy as np
 from Bio import Swissprot
-def convert_uniprot_to_gene_name(uniprot_ids):
- uniprot_to_gene={
- 'ID': 'Gene Name 1',
-  'ID': 'Gene Name 2'
- }
- return {uniprot_id: uniprot_to_gene.get(uniprot_id) for uniprot_id in uniprot_ids}
+def getting_gene_name(Uniprot_ID):
 
 def convert_to_unweighted_graph(G):
  unweighted_G = nx.DiGraph()
