59d1533a9cabde128ee9fb2eea44e822079d644e
mairoze <maiteleb830@gmail.com>
2024-06-27 19:41:44 +0300

    importing Swissprot module from biopython that is retreiving protein annotation such protein name

--- src/PPINA/CTGN.py
+++ src/PPINA/CTGN.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import numpy as np
+from Bio import Swissprot
 def convert_uniprot_to_gene_name(uniprot_ids):
  uniprot_to_gene={
  'ID': 'Gene Name 1',
@@ -8,18 +9,21 @@ def convert_uniprot_to_gene_name(uniprot_ids):
  return {uniprot_id: uniprot_to_gene.get(uniprot_id) for uniprot_id in uniprot_ids}
 
 def convert_to_unweighted_graph(G):
- unweighted_G = nx.Graph()
+ unweighted_G = nx.DiGraph()
  for edge in G.edges():
   unweighted_G.add_edge(edge[0], edge[1])
   return unweighted_G
 
 def convert_to_adjacency_matrix(G):
- adj_matrix = nx.to_numpy_array(G)
- return adj_matrix
+ ad_matrix = nx.to_numpy_array(G)
+ return ad_matrix
 
 
-def save_adjacency_matrix(adj_matrix, filename):
- np.savetxt(filename, adj_matrix, delimiter=',', fmt='%d')
+def save_adjacency_matrix(ad_matrix, filename):
+ np.savetxt(filename, ad_matrix, delimiter=';', fmt='%d')
+
+
+
 
 
 
