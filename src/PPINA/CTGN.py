8f6028c4a5a5700ae266f4878bfdc21551dae047
mairoze <maiteleb830@gmail.com>
2024-06-29 00:13:47 +0300

    Looping through each (UniProt ID) in the provided list Uniprot_IDs.that u have

--- src/PPINA/CTGN.py
+++ src/PPINA/CTGN.py
@@ -4,21 +4,17 @@ from Bio import Entrez, SeqIO
 
 Entrez.email = "maiteleb830@gmail.com"
 
-def getting_gene_name(Uniprot_ID):
+def getting_gene_name(Uniprot_IDs):
+    Gene_names = []
+    for ID in Uniprot_IDs:
+        Prot = Entrez.efetch(db="protein", id=ID, rettype="gb", retmode="text")
+        result = SeqIO.read(Prot, "gb")
+        Gene_names.append(result.description.split()[1])
+    return gene_names
 
-    Prot = Entrez.efetch(db="protein", id=Uniprot_ID, rettype="gb", retmode="text")
-    result = SeqIO.read(Prot, "gb")
-    return result.description.split()[1]
-#
 # testing function 1
-Uniprot_IDs=['Q8TBF4','Q15717','Q5MIZ7','Q8N490']
-for Uniprot_ID in Uniprot_IDs:
-    Gene_Name = getting_gene_name(Uniprot_ID)
-    if Gene_Name is not None:
-        print(f"UniProt ID: {Uniprot_ID}, Gene Name: {Gene_Name}")
-    else:
-        print(f"Error: Invalid UniProt ID: {Uniprot_ID}")
-
+Uniprot_IDs = ["Q8TBF4", "Q6I9Y2", "P51648","Q5MIZ7"]
+print(getting_gene_name(Uniprot_IDs))
 
 
 def convert_to_unweighted_graph(G):
@@ -26,7 +22,7 @@ def convert_to_unweighted_graph(G):
     unweighted_G.add_edges_from(G.edges())
     return unweighted_G
 
-#test function
+#test function2
     G = nx.DiGraph()
     G.add_edge('A', 'B', weight=3.1)
     G.add_edge('B', 'C', weight=7.1)
@@ -37,13 +33,13 @@ def convert_to_unweighted_graph(G):
 def convert_to_adjacency_matrix(G):
  ad_matrix = nx.to_numpy_array(G)
  return ad_matrix
-#test function
+#test function3
  ad_matrix = convert_to_adjacency_matrix(unweighted_G)
 
 def save_adjacency_matrix(ad_matrix, filename):
  np.savetxt(filename, ad_matrix, delimiter=';', fmt='%d')
 
-#test function
+#test function4
 save_adjacency_matrix(ad_matrix, 'ad45_matrixx.csv')
 
 print(ad_matrix)
