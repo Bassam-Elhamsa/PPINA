410ae57b0ead001110ae241864538327b4daf2ab
mahmoudn1999 <M.Nashaat2278@nu.edu.eg>
2024-06-29 03:04:53 +0300

    Removing the "result saving" code and adding the constructed graph code.

--- src/PPINA/graph_construction.py
+++ src/PPINA/graph_construction.py
@@ -1,9 +1,10 @@
 import csv
 import os
+import networkx as nx
+import matplotlib.pyplot as plt
 
 # reading the file and make a list of tuples
 file_path = "/home/work/PathLinker_2018_human-ppi-weighted-cap0_75.txt"
-output_file_path = "/home/work/output.txt"
 
 if not os.path.isfile(file_path):
     raise FileNotFoundError("Please provide a valid input file path, or make sure the file exists.")
@@ -22,19 +23,23 @@ def read_file(file_path, delimiter="\t"):
         return list_of_tuples
     except FileNotFoundError:
         print("Input file is missing.")
-
-# save the output
-def save_list(data, output_file_path, delimiter="\t"):
-    with open(output_file_path, 'w') as output_file:
-        write_output = csv.writer(output_file, delimiter = delimiter)
-        write_output.writerow(["#tail", "head", "edge_weight"])
-        write_output.writerows(data)
+    return read_file
+print(read_file(file_path)[0:20])      
+        
+#Draw a constructed graph
+def draw_graph(file_path, delimiter="\t"):
+    G = nx.DiGraph()
+    G.add_weighted_edges_from(read_file(file_path)[0:20], delimiter = delimiter)
+    positions = nx.spring_layout(G)
+    nx.draw_networkx_nodes(G, positions, node_size=1500)
+    nx.draw_networkx_edges(G, positions, edgelist=G.edges, edge_color='k', width=1)
+    nx.draw_networkx_labels(G, positions, font_size=10, font_family='sans-serif')
+    plt.show()
+    return draw_graph
 
 
 try:
-    output = read_file(file_path)
-    save_list(output, output_file_path)
-except FileNotFoundError:
-    print("Output file is missing.")
+    draw_graph(file_path)
 except Exception as e:
     print(f"An error occurred: {e}")
+    
