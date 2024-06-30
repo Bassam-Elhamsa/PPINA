3d25fe4865e565fd31dd3c647c1443c2d6fc0798
Bassam Elhamsa <bassamadel663@gmail.com>
2024-06-30 23:20:30 +0300

    Update CTGN.py
    Remove tests and adding parameter Entrez_email to getting_gene_name function

--- src/PPINA/CTGN.py
+++ src/PPINA/CTGN.py
@@ -2,9 +2,10 @@ import networkx as nx
 import numpy as np
 from Bio import Entrez, SeqIO
 
-Entrez.email = "maiteleb830@gmail.com"
 
-def getting_gene_name(Uniprot_IDs):
+
+def getting_gene_name(Uniprot_IDs,Entrez_email):
+    Entrez.email = Entrez_email
     Gene_names = []
     for ID in Uniprot_IDs:
         Prot = Entrez.efetch(db="protein", id=ID, rettype="gb", retmode="text")
@@ -12,9 +13,6 @@ def getting_gene_name(Uniprot_IDs):
         Gene_names.append(result.description.split()[1])
     return gene_names
 
-# testing function 1
-Uniprot_IDs = ["Q8TBF4", "Q6I9Y2", "P51648","Q5MIZ7"]
-print(getting_gene_name(Uniprot_IDs))
 
 
 def convert_to_unweighted_graph(G):
@@ -22,27 +20,16 @@ def convert_to_unweighted_graph(G):
     unweighted_G.add_edges_from(G.edges())
     return unweighted_G
 
-#test function2
-    G = nx.DiGraph()
-    G.add_edge('A', 'B', weight=3.1)
-    G.add_edge('B', 'C', weight=7.1)
-    G.add_edge('C', 'A', weight=3.11)
-    unweighted_G = convert_to_unweighted_graph(G)
-    print(unweighted_G)
+
 
 def convert_to_adjacency_matrix(G):
  ad_matrix = nx.to_numpy_array(G)
  return ad_matrix
-#test function3
- ad_matrix = convert_to_adjacency_matrix(unweighted_G)
+
 
 def save_adjacency_matrix(ad_matrix, filename):
  np.savetxt(filename, ad_matrix, delimiter=';', fmt='%d')
 
-#test function4
-save_adjacency_matrix(ad_matrix, 'ad45_matrixx.csv')
-
-print(ad_matrix)
 
 
 
