67f5aa429323c23701b3c1a829ac6b74b25b8912
mahmoudn1999 <M.Nashaat2278@nu.edu.eg>
2024-06-30 23:48:17 +0300

    editing the script to contain only functions to prepare it to convert into a package, editing the subnetwork graph to show the acyclic shortest paths, and removing the os function.

--- src/PPINA/graph_construction.py
+++ src/PPINA/graph_construction.py
@@ -1,45 +1,35 @@
-import os
 import networkx as nx
 import matplotlib.pyplot as plt
 
-# reading the file and make a list of tuples
-file_path = "/home/work/PathLinker_2018_human-ppi-weighted-cap0_75.txt"
-source="P29218"
-target="Q13315"
-
-if not os.path.isfile(file_path):
-    raise FileNotFoundError("Please provide a valid input file path, or make sure the input file exists.")
-
-def read_file(file_path):
+def G_build(file_path):
     try:
         list_of_tuples = []
         with open(file_path, "r") as file:
-            data = file.readlines()
-            for line in data:
-                if line[0] == '#':                    
+            for line in file.readlines():
+                if line[0] == '#':
                     continue
                 line = line.split("\t")
                 list_of_tuples.append((line[0], line[1], float(line[2])))
-        return list_of_tuples
+        G = nx.DiGraph()
+        G.add_weighted_edges_from(list_of_tuples)
+        return G
     except FileNotFoundError:
         print("Input file is missing.")
-    return read_file
-print(f"{read_file(file_path)[0:5]}\n")      
-        
-#Making a directed graph
-graph = nx.DiGraph()
-graph.add_weighted_edges_from(read_file(file_path))
 
+
+
+
+def all_shortest_paths(graph,source,target):
 # Find the shortest path between two proteins
-paths = nx.all_shortest_paths(graph, source=source, target=target)
-paths_list = list(paths)  # Convert to a list to reuse
+    paths = nx.all_shortest_paths(graph, source, target)
+    paths_list = list(paths)  # Convert to a list to reuse
+    return paths_list
 
-print(f"The shortest path is: {paths_list}\n")
 
-# Calcultae the weight of the shortest paths
-def find_shortest_paths(graph, source, target):
+# Calculate the weight of the shortest paths
+def weight_of_each_path(graph, source, target,file_name="shortest_paths_with_weights"):
     path_details = []
-    paths = nx.all_shortest_paths(graph, source=source, target=target)
+    paths = all_shortest_paths(graph, source=source, target=target)
     for path in paths:
         path_score = 0
         edge_weights = []
@@ -48,20 +38,29 @@ def find_shortest_paths(graph, source, target):
             path_score += weight
             edge_weights.append(weight)
         path_details.append((path, path_score, edge_weights))
+    with open(file_name,'w') as out:
+        out.write("#Path\tPath_Score\tInteraction_Weights\n")
+        for path in path_details:
+            Path = "--".join(path[0])
+            Path_score = float(path[1])
+            Interaction_weights = str(path[2])
+            out.write("%s\t%f\t%s\n" %(Path,Path_score,Interaction_weights))
     return path_details
 
-path_details = find_shortest_paths(graph, source, target)
-print(f"the weight of each path is: {path_details}\n")
 
-# Calculate the total path score for the shortest paths
-total_path_score = sum([path_score for _, path_score, _ in path_details])
-print(f"The total path score for the shortest paths is: {total_path_score}\n")
 
-# draw the sub-network with the shortest path
-shortest_path = paths_list[0]
-shortest_path_graph = graph.subgraph(shortest_path)
-position = nx.spring_layout(shortest_path_graph)
-nx.draw(shortest_path_graph, position, with_labels=True, node_size=600, node_color='skyblue', edge_color='yellow')
-labels = nx.get_edge_attributes(shortest_path_graph, 'weight')
-nx.draw_networkx_edge_labels(shortest_path_graph, position, edge_labels=labels)
-plt.show()
+# Extract the edges and nodes for the subgraph
+def subgraph_plot(G,path):
+    edges = []
+    for i in range(len(path)-1):
+        edges.append((path[i],path[i+1]))
+    subgraph = G.edge_subgraph(edges)
+    # Draw the subgraph
+    position = nx.spring_layout(subgraph)  # Layout for better visualization
+    nx.draw(subgraph, position, with_labels=True, node_size=1500, node_color="skyblue",
+            font_size=12, font_weight="bold", edge_color="cyan", width=2.0)
+    labels = nx.get_edge_attributes(subgraph, 'weight')
+    nx.draw_networkx_edge_labels(subgraph, position, edge_labels=labels)
+    plt.show()
+
+
